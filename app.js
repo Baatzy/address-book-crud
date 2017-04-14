7bf0129e1ff7aa80ec76d102268167c96ce1f5ad
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-04-14 15:41:06 -0700

    Finished homepage -Added navbar partial -Added method-override into app.js

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var hbs = require('hbs')
+var methodOverride = require('method-override')
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var contacts = require('./routes/contacts');
 
 var app = express();
 
@@ -13,14 +15,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(methodOverride('_method'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/contacts', contacts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
